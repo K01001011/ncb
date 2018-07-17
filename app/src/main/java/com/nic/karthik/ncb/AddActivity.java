dfd6bbaec8af4edd5eb687588ff912e00b569cbc
vijaykoundinya <41119479+vijaykoundinya@users.noreply.github.com>
2018-07-17 16:07:28 +0530

    x
    x

--- app/src/main/java/com/nic/karthik/ncb/AddActivity.java
+++ app/src/main/java/com/nic/karthik/ncb/AddActivity.java
@@ -70,20 +70,13 @@ public class AddActivity extends AppCompatActivity {
             }
         });
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
                 return;
-            } else {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
             }
+        else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+             }
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
